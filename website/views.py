31b2951fe8662b07f6229c984c968a66db0ac059
AndrewB <avborovets@gmail.com>
2022-07-12 18:38:21 +0300

    start template

--- website/views.py
+++ website/views.py
@@ -1,8 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-	return "<h1> test </h1>"
+	return render_template("home.html")
 
+
+# @views.route('/info')
+# def home():
+# 	return render_template("info.html")
