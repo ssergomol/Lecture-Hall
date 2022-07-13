60dbd63273826f3a44e3e8703c23afc1ae263f9b
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-07-13 23:21:24 +0300

    done users' auth

---
+++ website/models.py
@@ -0,0 +1,10 @@
+from . import db 
+from flask_login import UserMixin 
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    username = db.Column(db.String(150))
+    status = db.Column(db.String(150))
+    rank = db.Column(db.Integer)
--- website/views.py
+++ website/views.py
@@ -1,15 +1,18 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, logout_user, current_user
+
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-	return render_template("home.html")
+	return render_template("home.html", user=current_user)
 
 @views.route('/info')
 def info():
-    return render_template("info.html")
+    return render_template("info.html", user=current_user)
 
 @views.route('/profile')
+@login_required
 def profile():
-    return render_template("profile.html")
+    return render_template("profile.html", user=current_user)
