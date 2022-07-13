28e614c1088e2a09e11899a1b0c3042ef2259d39
AndrewB <avborovets@gmail.com>
2022-07-13 21:30:22 +0300

    creataion account done

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
+
 
 
 db = SQLAlchemy()
@@ -23,6 +25,14 @@ def create_app():
 	from .models import User, Publication
 
 	create_database(app)
+
+	login_manager = LoginManager()
+	login_manager.login_view = 'auth.login'
+	login_manager.init_app(app)
+
+	@login_manager.user_loader
+	def load_user(id):
+		return User.query.get(int(id))
 
 	return app
 
--- website/auth.py
+++ website/auth.py
@@ -59,11 +59,11 @@ def sign_up():
 		elif len(password1) < 7:
 			flash('Password must be at least 7 characters.', category='error')
 		else:
-			# new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-			#     password1, method='sha256'))
-			# db.session.add(new_user)
-			# db.session.commit()
-			# login_user(new_user, remember=True)
+			new_user = User(email=email, username=username, password=generate_password_hash(
+			    password1, method='sha256'))
+			db.session.add(new_user)
+			db.session.commit()
+			login_user(new_user, remember=True)
 			flash('Account created!', category='success')
 			return redirect(url_for('views.home'))
 
