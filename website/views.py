70d7cd810dc8e1f1fc0b4206565d1c55df2bb781
Sergey Molchanov <ser-mol2001@yandex.ru>
2022-07-14 18:42:36 +0300

    add dynamic left bar for lectures

--- website/views.py
+++ website/views.py
@@ -8,47 +8,56 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-	return render_template("home.html", user=current_user)
+	sections = Section.query.all()
+	parent = Section.query.filter_by(id=1).first()
+	return render_template("home.html", user=current_user, sections=sections, parent_section=parent)
 
 @views.route('/info')
 def info():
-    return render_template("info.html", user=current_user)
+	return render_template("info.html", user=current_user)
 
 @views.route('/profile')
 @login_required
 def profile():
-    return render_template("profile.html", user=current_user)
+	sections = Section.query.all()
+	parent = Section.query.filter_by(id=1).first()
+	return render_template("profile.html", user=current_user, sections=sections, parent_section=parent)
 
 @views.route('/add_lecture', methods=['GET', 'POST'])
 @login_required
 def add_lecture():
-    if request.method == 'POST':
-        title = request.form.get('title')
-        parent_title = request.form.get('parent-section')
-        data = request.form.get('lecture-text')        
-        
-        section = Section.query.filter_by(title=title).first()
-        parent_section = Section.query.filter_by(title=parent_title).first()
-        
-        if not (title and parent_title and data):
-            flash('Заполните все поля', category='error')
-        elif not parent_section:
-            flash('Категории с таким названием не существует', category='error')
-        elif section:
-            flash('Документ c таким названием уже существует', category='error')
-        elif len(data) < 1:
-            flash('Документ должен содержать хотя бы 1 символ', category='error')
-        else:
-            new_section = Section(parent_id=parent_section.id, title=title, 
-                                  data=data, user_id=current_user.id)
-            db.session.add(new_section)
-            db.session.commit()
-            flash('Новый документ успешно создан!', category='success')
-            for section in Section.query.all():
-                print(f'\n\n{section.id} {section.parent_id} {section.title} {section.data} {section.user_id}\n')
-                print(f"{section.title} - Children: {section.children}\n\n")
-            return redirect(url_for('views.profile'))
-        
-        
-        
-    return render_template("add_lecture.html", user=current_user)
+	if request.method == 'POST':
+		title = request.form.get('title')
+		parent_title = request.form.get('parent-section')
+		data = request.form.get('lecture-text')        
+		
+		section = Section.query.filter_by(title=title).first()
+		parent_section = Section.query.filter_by(title=parent_title).first()
+		
+		if not (title and parent_title and data):
+			flash('Заполните все поля', category='error')
+		elif not parent_section:
+			flash('Категории с таким названием не существует', category='error')
+		elif section:
+			flash('Документ c таким названием уже существует', category='error')
+		elif len(data) < 1:
+			flash('Документ должен содержать хотя бы 1 символ', category='error')
+		else:
+			new_section = Section(parent_id=parent_section.id, title=title, 
+								  data=data, user_id=current_user.id)
+			db.session.add(new_section)
+			db.session.commit()
+			flash('Новый документ успешно создан!', category='success')
+			sections = Section.query.all()
+			for section in sections:
+				print(f'\n\n{section.id} {section.parent_id} {section.title} {section.data} {section.user_id}\n')
+				print(f"{section.title} - Children: {section.children}\n\n")
+			return redirect(url_for('views.profile'))
+
+	sections = Section.query.all()
+	parent = Section.query.filter_by(id=1).first()
+	return render_template("add_lecture.html", user=current_user, sections=sections, parent_section=parent)
+
+@views.route("`/lectures/<id>`", methods=['GET'])
+def lectures(id):
+    return render_template("lecture.html", )
